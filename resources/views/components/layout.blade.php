40389bf2eb0af35672d1e319bbdd0e3baa39efc7
carlospinheiro <cadutapinheiro@gmail.com>
2025-03-17 16:56:26 -0300

    flash messages

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,14 @@
 
     @vite('resources/css/app.css')
 </head>
-<body>
+<body class="!h-screen overflow-auto">
+
+    @if (session('success'))
+        <div id="flash" class="p-4 text-center bg-green-50 text-green-500 font-bold">
+        {{ session('success') }}
+        </div>
+    @endif
+
     <header>
         <nav>
             <h1>Ninja Network</h1>
@@ -15,8 +22,20 @@
             <a href="{{ route('ninjas.create') }}">Create New Ninja</a>
         </nav>
     </header>
-    <main class="container !min-h-screen !overflow-auto">
+    <main class="container h-[calc(100vh_-_100px)] overflow-auto">
         {{ $slot }}
     </main>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+        var flash = document.getElementById('flash');
+        
+        if (flash) {
+            setTimeout(function () {
+            flash.remove(); 
+            }, 2000);
+        }
+        });
+    </script>
 </body>
 </html>
